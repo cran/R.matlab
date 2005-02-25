d4b831091c7009b641a7d08dccf41ab9fa59e5ed
Henrik Bengtsson <henrikb@braju.com>
2005-02-25 00:00:00 +0000

    version 1.0

---
+++ R/006.fixVarArgs.R
@@ -0,0 +1,11 @@
+# Added '...' to some base functions. These will later be
+# turned into default functions by setMethodS3().
+
+isOpen <- appendVarArgs(isOpen)
+
+
+############################################################################
+# HISTORY:
+# 2005-02-15
+# o Created to please R CMD check.
+############################################################################
---
+++ R/999.NonDocumentedObjects.R
@@ -0,0 +1,58 @@
+###########################################################################/**
+# @RdocDocumentation "Non-documented objects"
+#
+# @alias "cat"
+# @alias "cat.default"
+# @alias "evaluate"
+# @alias "getThreshold"
+# @alias "getVariable"
+# @alias "isVisible"
+# @alias "newline"
+# @alias "printf"
+# @alias "readResult"
+# @alias "ruler"
+# @alias "setFunction"
+# @alias "setThreshold"
+# @alias "setVariable"
+# @alias "setVerbose"
+# @alias "startServer"
+# @alias "writeCommand"
+#
+# @alias "asByte"
+# @alias "asInt"
+# @alias "asLong"
+# @alias "asShort"
+# @alias "isOpen"
+# @alias "isOpen.default"
+# @alias "readByte"
+# @alias "readInt"
+# @alias "readShort"
+# @alias "readUTF"
+# @alias "writeByte"
+# @alias "writeInt"
+# @alias "writeShort"
+# @alias "writeUTF"
+#
+# \description{
+#   This page contains aliases for all "non-documented" objects that 
+#   \code{R CMD check} detects in this package. 
+#
+#   Almost all of them are \emph{generic} functions that have specific 
+#   document for the corresponding method coupled to a specific class. 
+#   Other functions are re-defined by \code{setMethodS3()} to 
+#   \emph{default} methods. Neither of these two classes are non-documented
+#   in reality.
+#   The rest are deprecated methods.
+# }
+#
+# @author
+#
+# @keyword internal
+#*/###########################################################################
+
+############################################################################
+# HISTORY:
+# 2005-02-10
+# o Created to please R CMD check.
+############################################################################
+
